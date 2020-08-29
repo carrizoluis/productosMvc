7f8a4e27722761071f74128696d64f41f2e9e815
Luis Alberto Carrizo <carry_90@hotmail.com>
2020-08-29 09:50:12 -0300

    First commit

---
+++ productosMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace productosMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
