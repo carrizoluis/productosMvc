439530d0164ff97150e95a910d81dc48055cf9be
Luis Alberto Carrizo <carry_90@hotmail.com>
2020-08-29 14:58:11 -0300

    Proyecto DB.Entidades

---
+++ DB.Entidades/DbConnection/DbConnection.cs
@@ -0,0 +1,32 @@
+﻿using DB.Entidades.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB.Entidades.DbConnection
+{
+    public class DbConnection : DbContext
+    {
+        private IConfiguration Configuration;
+        public virtual DbSet<Producto> Producto { get; set; }
+
+        public DbConnection(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        //Sobreescribimos el metodo OnConfiguring para hacer los ajustes que queramos en caso de
+        //llamar al constructor sin parametros
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //En caso de que el contexto no este configurado, lo configuramos mediante la cadena de conexion
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(Configuration["StringConnection"]);
+
+            }
+        }
+    }
+}
---
+++ DB.Entidades/Model/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DB.Entidades.Model
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int? IdProducto { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string CodigoBarra { get; set; }
+        public string Nombre { get; set; }
+        public double PrecioCosto { get; set; }
+        public double Iva { get; set; }
+        public double Ganancia { get; set; }
+        public Boolean borrado { get; set; }
+    }
+}
